c46a9faf97a976108512589d21adfc5cdfd510c6
boscha1 <bosch.anthony7@gmail.com>
2020-11-28 12:58:59 -0500

    added DB info

--- settings.php
+++ settings.php
@@ -6,16 +6,27 @@
 	//		- usability (many helper functions to automate routine operations)
 	//		- reusability (unified API to access multitude of databases, from SQLite to Oracle)
 
-$host = 'localhost';
-$db = 'midtermdb';
-$user = 'root';
-$pass = '';
-$charset = 'utf8';
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-echo('<pre>');
-if ($_SERVER['SERVER_NAME']=='localhost') 
+$server = $url["host"];
+echo $server;
+die();
+
+if ($_SERVER['SERVER_NAME']=='localhost') {
+	$host = 'localhost';
+	$db = 'midtermdb';
+	$user = 'root';
+	$pass = '';
+	$charset = 'utf8';
 	$base_URL = 'http://localhost:8080/Final%20Project/';
-else
+}
+else {
+	$host = 'localhost';
+	$db = 'heroku_1ad6155d62809e0';
+	$user = 'b533c8afed0280';
+	$pass = 'f199a65e ';
+	$charset = 'utf8';
 	$base_URL = 'https://nku-car-site.herokuapp.com/';
+}
 
 session_start();

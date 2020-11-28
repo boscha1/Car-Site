5fd78fd99be875f28f495acd57317edc328e25b9
boscha1 <bosch.anthony7@gmail.com>
2020-11-28 13:13:56 -0500

    fixed database info

--- settings.php
+++ settings.php
@@ -6,11 +6,7 @@
 	//		- usability (many helper functions to automate routine operations)
 	//		- reusability (unified API to access multitude of databases, from SQLite to Oracle)
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-echo $server;
-die();
+$charset = 'utf8';
 
 if ($_SERVER['SERVER_NAME']=='localhost') {
 	$host = 'localhost';
@@ -21,12 +17,12 @@ if ($_SERVER['SERVER_NAME']=='localhost') {
 	$base_URL = 'http://localhost:8080/Final%20Project/';
 }
 else {
-	$host = 'localhost';
-	$db = 'heroku_1ad6155d62809e0';
-	$user = 'b533c8afed0280';
-	$pass = 'f199a65e ';
-	$charset = 'utf8';
-	$base_URL = 'https://nku-car-site.herokuapp.com/';
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$host = $url["host"];
+	$db = substr($url["path", 1]);
+	$user = $url["user"];
+	$pass = $url["pass"];
+	//$base_URL = 'https://nku-car-site.herokuapp.com/';
 }
 
 session_start();

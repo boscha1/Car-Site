efbac6660ae670a50b7ac2763c64cb56e6c68a26
boscha1 <bosch.anthony7@gmail.com>
2020-11-28 12:40:00 -0500

    settings change

--- settings.php
+++ settings.php
@@ -16,6 +16,6 @@ echo('<pre>');
 if ($_SERVER['SERVER_NAME']=='localhost') 
 	$base_URL = 'http://localhost:8080/Final%20Project/';
 else
-	$base_URL = https://nku-car-site.herokuapp.com/
+	$base_URL = 'https://nku-car-site.herokuapp.com/';
 
 session_start();
